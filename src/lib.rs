417c072bbd9aedac57d6fecb61e4cb9e0ca51788
andyzyao <andyzyao@vip.qq.com>
2021-06-24 20:30:55 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub use gettid::gettid;
 
 #[macro_export]
 macro_rules! message {
-    ($($arg:tt)*) => {
+    ($($arg:tt)*) => {{
         use std::fs::OpenOptions;
         use std::io::Write;
 
@@ -13,7 +13,7 @@ macro_rules! message {
         if let Ok(mut f) = OpenOptions::new().write(true).append(true).create(true).open("/tmp/debug.txt") {
             writeln!(f, "{}", s).unwrap();
         }
-    }
+    }}
 }
 
 #[macro_export]
