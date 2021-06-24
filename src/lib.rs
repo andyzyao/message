94b2ee330c727f4239f999cdd5ef7a784a359a62
andyzyao <andyzyao@vip.qq.com>
2021-06-24 20:41:50 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ macro_rules! message {
         let mut s = String::from(format!("[{} {} {}:{}] ", $crate::Local::now().format("%Y-%m-%d %H:%M:%S%.3f"), $crate::gettid(),  file!(), line!()));
         s += &String::from(format!($($arg)*));
 
-        if let Ok(mut f) = OpenOptions::new().write(true).append(true).create(true).open("/tmp/debug.txt") {
+        if let Ok(mut f) = OpenOptions::new().append(true).create(true).open("/tmp/debug.txt") {
             writeln!(f, "{}", s).unwrap();
         }
     }}
